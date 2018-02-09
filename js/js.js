1a3886aac44dce8cc1129da3c637874ae5688d44
Joey <23112101+gfrcsd@users.noreply.github.com>
2018-02-09 15:21:14 +0000

    Added artist page content

--- js/js.js
+++ js/js.js
@@ -58,3 +58,22 @@ $(document).ready(function(){
 	});
 	
 });
+
+// T A B S
+
+function openBand(evt, bandName) {
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    document.getElementById(bandName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
