9a8eb0b772f976ce9670c2b48b959e3dc18ce2a0
Louise <30032093@nescol.ac.uk>
2018-05-27 20:07:09 +0100

    Mobile Version Complete (#17)
    Optimised for mobiles and tablets as well as @zSaibot’s logo.

--- js/js.js
+++ js/js.js
@@ -1,8 +1,139 @@
-// JavaScript Document
+$(document).ready(function(){
+	$(".loader").delay(1000).fadeOut("200");
+	$("#load").delay(1500).fadeOut("400");
+});
+
+function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+});// JavaScript Document
 $(document).ready(function(){
 	"use strict";
 	
-	$("#site-id, #hm").click(function(){
+	$(".small-logo").hide();
+	
+	$(".dropdownmenu").hide();
+	
+	$("#hm").click(function(){
+		
+		$("#home").show();
 		
 		$("#about").animate({left:'100%'});
 		
@@ -10,15 +141,35 @@ $(document).ready(function(){
 		
 		$("#event").animate({right:"100%"});
 		
-		$("#menu").removeClass("change");
+		$(".menu").removeClass("change crosspadding");
 		
 		$(".overlay").removeClass("over");
 		
-		$("#site-id").fadeOut();
+		$(".small-logo img").hide();
+		
+	});
+	
+	$(".small-logo img").click(function(){
+		
+		$("#home").show();
+		
+		$("#about").animate({left:'100%'});
+		
+		$("#artists").animate({top:"100%"});
+		
+		$("#event").animate({right:"100%"});
+		
+		$(".menu").removeClass("change");
+		
+		$(".overlay").removeClass("over");
+		
+		$(".small-logo img").hide();
 		
 	});
 	
 	$("#ab").click(function(){
+		
+		$("#about").show();
 		
 		$("#about").animate({left:'0'});
 		
@@ -26,23 +177,30 @@ $(document).ready(function(){
 		
 		$("#event").animate({right:"100%"});
 		
-		$("#menu").removeClass("change");
+		$(".menu").removeClass("change");
 		
 		$(".overlay").removeClass("over");
 		
+		$(".small-logo img").show();
+		
 	});
 	
-	$("#menu").click(function(){
+		$(".menu").click(function(){
+			
+			$(this).toggleClass("change");
 		
-		$(this).toggleClass("change");
-		
-		$(".overlay").toggleClass("over");
-		
-	});
+			$(".overlay").toggleClass("over");
+			
+			$(this).toggleClass("crosspadding");
+			
+		});
 	
 	$("#start").click(function(){
-		
+		$("#about").show();
 		$("#about").animate({left:'0'});
+		$(".small-logo").show();
+		$(".small-logo img").show();
+		$(".menu").show();
 		
 		$("#site-id").fadeIn();
 		
@@ -50,18 +208,22 @@ $(document).ready(function(){
 	
 	$("#art").click(function(){
 		
+		$("#artists").show();
+		
 		$("#artists").animate({top:"0"});
 		
 		$("#about").animate({left:"100%"});
 		
 		$("#event").animate({right:"100%"});
 		
-		$("#menu").removeClass("change");
+		$(".menu").removeClass("change crosspadding");
 		
 		$(".overlay").removeClass("over");
 	});
 	
 	$("#ev").click(function(){
+		
+		$("#event").show();
 		
 		$("#event").animate({right:"0"});
 		
@@ -69,7 +231,7 @@ $(document).ready(function(){
 		
 		$("#about").animate({left:"100%"});
 		
-		$("#menu").removeClass("change");
+		$(".menu").removeClass("change crosspadding");
 		
 		$(".overlay").removeClass("over");
 		
@@ -77,14 +239,194 @@ $(document).ready(function(){
 	
 	$("#down").click(function(){
 		
+		$("#artists").show();
+		
 		$("#artists").animate({top:"0"});
 		
 	});
 	
 	$("#left").click(function(){
 		
+		$("#event").show();
+		
 		$("#event").animate({right:"0"});
 		
+	});
+	
+	// M O B I L E
+
+$("#mobaboutarrow").click(function(){
+		
+		$("#about").hide();
+		
+		$("#artists").show();
+	
+		$(".small-logo").show();
+	
+		$(".tab").hide();
+	
+		$(".menu").show();
+	
+		$("#event").animate({right:"0"});
+	
+		$(".small-logo img").off('click');
+	
+			$(".small-logo img").click(function(){
+
+				$("#artists").hide();
+
+				$("#event").hide();
+
+				$("#about").show();
+
+				$(".menu").removeClass("change crosspadding");
+
+				$(".overlay").removeClass("over");
+
+		});
+	
+	});
+	
+$(".left-col h4").click(function(){
+		
+		$(".dropdownmenu").toggle();
+	
+		$(".left-col img, .right-col, .arrow").toggle();
+	
+		$("#left").hide();
+	
+		$("#Artist1, #Artist2, #Artist3, #Artist4, #Artist5, #Artist6").toggleClass("artistpadding");
+		
+	});
+	
+$(".dropdownmenu p").click(function(){
+		
+		$("#Artist1, #Artist2, #Artist3, #Artist4, #Artist5, #Artist6").removeClass("artistpadding");
+		$(".left-col img, .right-col, .arrow").toggle();
+		$("#left").hide();
+		
+	});
+	
+$(".katie").click(function(){
+		
+		$("#Artist1").show();
+		$("#Artist2").hide();
+		$("#Artist3").hide();
+		$("#Artist4").hide();
+		$("#Artist5").hide();
+		$("#Artist6").hide();
+		$(".dropdownmenu").hide();
+		
+	});
+	
+$(".shannon").click(function(){
+		
+		$("#Artist2").show();
+		$("#Artist1").hide();
+		$("#Artist3").hide();
+		$("#Artist4").hide();
+		$("#Artist5").hide();
+		$("#Artist6").hide();
+		$(".dropdownmenu").hide();
+		
+	});
+	
+$(".jamie").click(function(){
+		
+		$("#Artist3").show();
+		$("#Artist1").hide();
+		$("#Artist2").hide();
+		$("#Artist4").hide();
+		$("#Artist5").hide();
+		$("#Artist6").hide();
+		$(".dropdownmenu").hide();
+		
+	});
+	
+$(".josh").click(function(){
+		
+		$("#Artist4").show();
+		$("#Artist1").hide();
+		$("#Artist2").hide();
+		$("#Artist3").hide();
+		$("#Artist5").hide();
+		$("#Artist6").hide();
+		$(".dropdownmenu").hide();
+		
+	});
+	
+$(".red").click(function(){
+		
+		$("#Artist5").show();
+		$("#Artist1").hide();
+		$("#Artist2").hide();
+		$("#Artist3").hide();
+		$("#Artist4").hide();
+		$("#Artist6").hide();
+		$(".dropdownmenu").hide();
+		
+	});
+	
+$(".glass").click(function(){
+		
+		$("#Artist6").show();
+		$("#Artist1").hide();
+		$("#Artist2").hide();
+		$("#Artist3").hide();
+		$("#Artist4").hide();
+		$("#Artist5").hide();
+		$(".dropdownmenu").hide();
+		
+	});
+	
+$(".mobartistarrow").click(function(){
+		
+		$("#artists").hide();
+	
+		$("#event").show();
+		
+	});
+	
+$(".hm-mobile").click(function(){
+	
+		$("#artists").hide();
+		
+		$("#event").hide();
+	
+		$("#about").show();
+	
+		$(".menu").removeClass("change crosspadding");
+		
+		$(".overlay").removeClass("over");
+	
+	});
+	
+$("#art").click(function(){
+	
+		$("#about").hide();
+	
+		$("#event").hide();
+		
+		$("#artists").show();
+	
+		$(".small-logo").show();
+	
+		$(".menu").show();
+	
+	});
+	
+$("#ev").click(function(){
+	
+		$("#about").hide();
+	
+		$("#event").show();
+		
+		$("#artists").hide();
+	
+		$(".small-logo").show();
+	
+		$(".menu").show();
+	
 	});
 	
 });
@@ -107,4 +449,6 @@ function openBand(evt, bandName) {
     document.getElementById(bandName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+// L O A D E R
 
